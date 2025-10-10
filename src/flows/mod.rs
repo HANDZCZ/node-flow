55d009283918ca4b27bc1ad2d912434deb7bb1d5
HANDZCZ <handz@email.cz>
2025-10-10 16:51:51 +0200

    Add flow impl helper types

--- src/flows/mod.rs
+++ src/flows/mod.rs
@@ -2,3 +2,8 @@ mod sequential_flow;
 pub use sequential_flow::*;
 mod one_of_sequential_flow;
 pub use one_of_sequential_flow::*;
+
+use crate::node::NodeOutput;
+type NodeIOE<Input, Output, Error> = (Input, NodeOutput<Output>, Error);
+type ChainLink<Head, Tail> = (Head, Tail);
+type NodeResult<Output, Error> = Result<NodeOutput<Output>, Error>;
