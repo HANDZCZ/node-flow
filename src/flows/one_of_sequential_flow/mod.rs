42a255b99f4d20fbede2e6b268cae7b55584852d
HANDZCZ <handz@email.cz>
2025-10-16 23:30:26 +0200

    Change OneOfSequentialFlow to use define macros

--- src/flows/one_of_sequential_flow/mod.rs
+++ src/flows/one_of_sequential_flow/mod.rs
@@ -1,5 +1,12 @@
-mod builder;
-pub use builder::*;
 mod chain_run;
-mod flow;
-pub use flow::*;
+
+use crate::flows::generic_defs::define_flow_and_ioe_conv_builder;
+use chain_run::ChainRunOneOfSequential as ChainRun;
+
+define_flow_and_ioe_conv_builder!(
+    OneOfSequentialFlow,
+    ChainRun,
+    >Input: Send + Clone,
+    #NodeType: Send + Sync + Clone
+    /// Docs :)
+);
