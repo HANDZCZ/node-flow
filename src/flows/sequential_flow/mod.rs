25d348a7970dfb6b9a0e9908d994f31537e1532f
HANDZCZ <handz@email.cz>
2025-10-16 23:32:00 +0200

    Add tests to SequentialFlow

--- src/flows/sequential_flow/mod.rs
+++ src/flows/sequential_flow/mod.rs
@@ -13,3 +13,40 @@ define_flow!(
     /// Docs :)
 );
 
+#[cfg(test)]
+mod test {
+    use super::{ChainRun, SequentialFlow as Flow};
+    use crate::{
+        flows::tests::Passer,
+        node::{Node, NodeOutput},
+        storage::Storage,
+    };
+
+    #[tokio::test]
+    async fn test_flow() {
+        let mut st = Storage::new();
+        let mut flow = Flow::<bool, u128, ()>::builder()
+            .add_node(Passer::<u8, u16, ()>::new())
+            .add_node(Passer::<u32, u64, ()>::new())
+            .build();
+        let res = flow.run_with_storage(true, &mut st).await;
+
+        assert_eq!(res, Ok(NodeOutput::Ok(1)));
+    }
+
+    #[tokio::test]
+    async fn test_chain() {
+        let mut st = Storage::new();
+        let node = (
+            (
+                (Passer::<bool, u8, ()>::new(),),
+                Passer::<u16, u32, ()>::new(),
+            ),
+            Passer::<u64, u128, ()>::new(),
+        );
+        let res =
+            ChainRun::<_, Result<NodeOutput<u128>, ()>, _>::run_with_storage(&node, true, &mut st)
+                .await;
+        assert_eq!(res, Ok(NodeOutput::Ok(1)));
+    }
+}
