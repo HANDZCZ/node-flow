e623405a5735323448676a36a985b2319bae0b53
HANDZCZ <handz@email.cz>
2025-10-16 23:32:16 +0200

    Add tests to OneOfSequentialFlow

--- src/flows/one_of_sequential_flow/mod.rs
+++ src/flows/one_of_sequential_flow/mod.rs
@@ -10,3 +10,57 @@ define_flow_and_ioe_conv_builder!(
     #NodeType: Send + Sync + Clone
     /// Docs :)
 );
+
+#[cfg(test)]
+mod test {
+    use super::{ChainRun, OneOfSequentialFlow as Flow};
+    use crate::{
+        flows::tests::{InsertIntoStorageAssertWasNotInStorage, Passer, SoftFailNode},
+        node::{Node, NodeOutput},
+        storage::Storage,
+    };
+
+    #[tokio::test]
+    async fn test_flow() {
+        let mut st = Storage::new();
+        let mut flow = Flow::<u8, u64, ()>::builder()
+            .add_node(SoftFailNode::<u16, u32, ()>::new())
+            .add_node(SoftFailNode::<u8, u16, ()>::new())
+            .add_node(SoftFailNode::<u32, u64, ()>::new())
+            .add_node(Passer::<u16, u32, ()>::new())
+            .build();
+        let res = flow.run_with_storage(5, &mut st).await;
+
+        assert_eq!(res, Ok(NodeOutput::Ok(5)));
+    }
+
+    #[tokio::test]
+    async fn test_chain() {
+        let mut st = Storage::new();
+        let node = (
+            (
+                (SoftFailNode::<u16, u32, ()>::new(),),
+                SoftFailNode::<u16, u32, ()>::new(),
+            ),
+            Passer::<u16, u32, ()>::new(),
+        );
+        let res =
+            ChainRun::<_, Result<NodeOutput<u64>, ()>, _>::run_with_storage(&node, 5u8, &mut st)
+                .await;
+        assert_eq!(res, Ok(NodeOutput::Ok(5)));
+    }
+
+    #[tokio::test]
+    async fn test_flow_storage() {
+        let mut st = Storage::new();
+        let mut flow = Flow::<u8, u64, ()>::builder()
+            .add_node(InsertIntoStorageAssertWasNotInStorage::<u16, u32, (), String>::new())
+            .add_node(InsertIntoStorageAssertWasNotInStorage::<u8, u16, (), String>::new())
+            .add_node(InsertIntoStorageAssertWasNotInStorage::<u32, u64, (), String>::new())
+            .add_node(Passer::<u16, u32, ()>::new())
+            .build();
+        let res = flow.run_with_storage(5, &mut st).await;
+
+        assert_eq!(res, Ok(NodeOutput::Ok(5)));
+    }
+}
