982499b9a3caa0f0b7c0c660e0350849817dc46e
HANDZCZ <handz@email.cz>
2025-10-16 23:28:15 +0200

    Change SequentialFlow to use define_flow macro

--- src/flows/sequential_flow/mod.rs
+++ src/flows/sequential_flow/mod.rs
@@ -1,5 +1,15 @@
 mod builder;
 pub use builder::*;
 mod chain_run;
-mod flow;
-pub use flow::*;
+
+use crate::flows::generic_defs::flow::define_flow;
+use chain_run::ChainRunSequential as ChainRun;
+
+define_flow!(
+    SequentialFlow,
+    ChainRun,
+    Input: Send,
+    Error: Send,
+    /// Docs :)
+);
+
