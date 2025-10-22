822cdfceaf81b6d53197312604041786351177b4
HANDZCZ <handz@email.cz>
2025-10-22 13:40:14 +0200

    Fix logic bug in Storage merge
    Items from other storages were considered changed if they weren't
    Fixes edge case when others slice was empty
    Adds more complex merging logic for skipping calling merge trait in simple merge cases

--- src/storage.rs
+++ src/storage.rs
@@ -141,28 +141,75 @@ impl Storage {
     /// Storages passed as others can be used later, but they will be missing all changed items
     /// (any item accessed through: [`get_mut`](Self::get_mut), [`remove`](Self::remove), [`insert`](Self::insert))!
     pub(crate) fn merge(&mut self, others: &mut [Self]) {
-        let mut changed = std::mem::take(&mut others[0].changed);
+        if others.is_empty() {
+            return;
+        }
+
+        // gather TypeId of all changed items
+        let mut changed = others[0].changed.clone();
         others
             .iter_mut()
             .skip(1)
-            .for_each(|s| changed.extend(s.changed.drain()));
+            .for_each(|s| changed.extend(s.changed.iter()));
 
         for key in changed {
+            // collect items from self and from other_items if the item was changed
             let parent = self.inner.get(&key).map(Box::as_ref);
             let other_items = others
                 .iter_mut()
-                .filter_map(|s| s.inner.remove(&key))
+                .filter_map(|s| {
+                    s.changed
+                        .remove(&key)
+                        .then(|| s.inner.remove(&key))
+                        .flatten()
+                })
                 .collect::<Box<[_]>>();
-            if other_items.is_empty() {
-                continue;
+
+            // decide if and how the items are merged
+            // allow match_same_arms for comments
+            #[allow(clippy::match_same_arms)]
+            match (parent.is_none(), other_items.is_empty()) {
+                // parent and other_items are empty
+                //     => item was inserted in a branch and then removed
+                // = skip item
+                (true, true) => continue,
+                // parent is empty and other_items contain exactly one item
+                //     => item was inserted in exactly one branch
+                //  or => item was inserted in multiple branches, but later it was removed from all but one branch
+                // = insert first and only item
+                (true, false) if other_items.len() == 1 => {
+                    let first = other_items.into_iter().next().unwrap();
+                    self.inner.insert(key, first);
+                    self.changed.insert(key);
+                    continue;
+                }
+                // parent is empty and other_items contain more than one item
+                //     => more than one branch inserted item
+                // = merge needed
+                (true, false) => {}
+                // parent is not empty and other_items is empty
+                //     => item was removed in all branches
+                // = remove item
+                (false, true) => {
+                    self.inner.remove(&key);
+                    self.changed.insert(key);
+                    continue;
+                }
+                // parent and other_items are not empty
+                //     => at least one branch inserted item
+                // = merge needed
+                (false, false) => {}
             }
+
+            // Merge trait is needed for merging
             let res = {
-                // Call merge on dyn StorageItem type
                 // All types (inside of a `parent` and `other_items[...]`) have the same type
                 let dispatcher: &dyn StorageItem = match parent {
                     Some(p) => p,
                     None => &*other_items[0],
                 };
+
+                // Call merge on dyn StorageItem type
                 // SAFETY: reference is only used for VTable lookup, the self type is otherwise unused,
                 //         this reference is then dropped and never used since it will most likely point to a non-existent data
                 let dispatcher: &dyn StorageItem = unsafe { &*std::ptr::from_ref(dispatcher) };
