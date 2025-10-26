b63f8035f2b74945e40b7330551a82f2682595e2
HANDZCZ <handz@email.cz>
2025-10-26 22:38:34 +0100

    Add features for storages

--- src/context/storage/local_storage/mod.rs
+++ src/context/storage/local_storage/mod.rs
@@ -1,4 +1,6 @@
+#[cfg(feature = "local_storage_impl")]
 mod implementation;
+#[cfg(feature = "local_storage_impl")]
 pub use implementation::*;
 mod design;
 pub use design::*;
--- src/context/storage/shared_storage/mod.rs
+++ src/context/storage/shared_storage/mod.rs
@@ -1,4 +1,6 @@
+#[cfg(feature = "shared_storage_impl")]
 mod implementation;
+#[cfg(feature = "shared_storage_impl")]
 pub use implementation::*;
 mod design;
 pub use design::*;
