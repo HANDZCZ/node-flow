4039e200c3a15963bfecd9e0da664ae4f1876a4e
HANDZCZ <handz@email.cz>
2025-10-28 00:24:36 +0100

    Add remove_generics_from_name helper fn

--- src/describe/mod.rs
+++ src/describe/mod.rs
@@ -1,2 +1,7 @@
 mod design;
 pub use design::*;
+
+pub(crate) fn remove_generics_from_name(orig_name: &mut String) {
+    let generic_start_idx = orig_name.find('<').unwrap_or(orig_name.len());
+    orig_name.truncate(generic_start_idx);
+}
