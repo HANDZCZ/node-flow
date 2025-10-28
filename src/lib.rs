a6404a0214e0842806f9c625e2e55a0b2f843a4a
HANDZCZ <handz@email.cz>
2025-10-28 17:50:20 +0100

    Fix merge all allow, warn and forbid attributes

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,19 @@
-//#![warn(missing_docs)]
-#![warn(unused_doc_comments)]
-#![warn(clippy::empty_docs)]
-#![warn(clippy::tabs_in_doc_comments)]
-#![warn(clippy::suspicious_doc_comments)]
-#![warn(clippy::test_attr_in_doctest)]
-#![warn(rustdoc::private_intra_doc_links)]
-#![warn(clippy::empty_line_after_doc_comments)]
-#![warn(clippy::pedantic)]
-#![forbid(rustdoc::broken_intra_doc_links)]
-#![forbid(missing_debug_implementations)]
-#![forbid(invalid_doc_attributes)]
+#![warn(
+    // missing_docs,
+    clippy::tabs_in_doc_comments,
+    clippy::suspicious_doc_comments,
+    clippy::test_attr_in_doctest,
+    clippy::empty_line_after_doc_comments,
+    unused_doc_comments,
+    clippy::empty_docs,
+    clippy::pedantic,
+    clippy::all,
+)]
+#![forbid(
+    rustdoc::broken_intra_doc_links,
+    missing_debug_implementations,
+    invalid_doc_attributes
+)]
 #![cfg_attr(all(doc, not(doctest)), feature(doc_cfg))]
 
 pub mod context;
