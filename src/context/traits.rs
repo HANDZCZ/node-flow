5506ebb698b62465d46f922751eaeeae8f17d85f
HANDZCZ <handz@email.cz>
2025-10-29 22:49:50 +0100

    Add traits for task spawning to context

--- src/context/traits.rs
+++ src/context/traits.rs
@@ -10,3 +10,22 @@ pub trait Update {
 pub trait Join: Sized {
     fn join(&mut self, others: Box<[Self]>);
 }
+
+pub trait Task<T>: Future<Output = T> {
+    fn is_finished(&self) -> bool;
+    fn cancel(self);
+}
+
+pub trait SpawnAsync {
+    fn spawn<F>(fut: F) -> impl Task<F::Output>
+    where
+        F: Future + Send + 'static,
+        F::Output: Send + 'static;
+}
+
+pub trait SpawnSync {
+    fn spawn_blocking<F, O>(func: F) -> impl Task<O>
+    where
+        F: Fn() -> O + Send + 'static,
+        O: Send + 'static;
+}
