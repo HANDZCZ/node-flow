2a6c526541902a299ea4773ecb4c2a10fe3cf871
HANDZCZ <handz@email.cz>
2025-11-02 11:39:48 +0100

    Add documentation to BoxedNode trait

--- src/node/boxed.rs
+++ src/node/boxed.rs
@@ -1,13 +1,48 @@
 use crate::{describe::Description, node::Node};
 
+/// The `BoxedNode` trait is a dyn compatible wrapper around the [`Node`] trait.
+///
+/// `BoxedNode` trait provides the same functions as the [`Node`] trait
+/// with the addition of dyn compatibility.
+/// This allows you to use `Box<dyn BoxedNode<...>>`.
+///
+/// Blanket implementation of [`BoxedNode`] is implemented
+/// for all types that implement [`Node`] trait.
+///
+/// See also [`Node`].
+///
+/// # Examples
+/// ```
+/// use node_flow::node::BoxedNode;
+///
+/// async fn run_node(
+///     node: &mut dyn BoxedNode<String, String, String, ()>,
+/// ) {
+///     let result = node.run_boxed("hello".into(), &mut ()).await;
+///     println!("{:?}", result);
+/// }
+/// ```
 #[async_trait::async_trait]
 pub trait BoxedNode<Input, Output, Error, Context> {
+    /// Runs the node.
+    ///
+    /// This method is equivalent to [`Node::run`], but allows calling it
+    /// via a `Box<dyn BoxedNode<...>>` trait object.
+    ///
+    /// # Returns
+    /// Boxed dyn [`Future`] (`Pin<Box<dyn Future<...> + Send + '_>>`) that resolves to a `Result<Output, Error>`.
+    ///
+    /// See also [`Node::run`].
     async fn run_boxed(&mut self, input: Input, context: &mut Context) -> Result<Output, Error>
     where
         Input: 'async_trait,
         Output: 'async_trait,
         Error: 'async_trait;
 
+    /// Describes this node, its type signature and other specifics.
+    ///
+    /// See [`Description`] for more details.
+    /// See also [`Node::describe`].
     fn describe(&self) -> Description;
 }
 
