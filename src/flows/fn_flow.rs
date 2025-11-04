691820c916d07e2ca4a706d0079f42531bff35dc
HANDZCZ <handz@email.cz>
2025-11-04 19:33:34 +0100

    Add documentation to FnFlow

--- src/flows/fn_flow.rs
+++ src/flows/fn_flow.rs
@@ -6,7 +6,18 @@ use crate::{
     node::{Node, NodeOutput},
 };
 
+/// The `Runner` is used in [`FnFlow`] and is basically a replacement for [`Node`].
+///
+/// It defines how `Input` and inner data is processed into an `Output` (and `Error`) with a given `Context`.
+///
+/// See also [`FnFlow`], [`Node`].
 pub trait Runner<'a, Input, Output, Error, Context, InnerData>: Send + Sync {
+    /// Executes the runner using the provided inner data, input, and context.
+    ///
+    /// # Parameters
+    /// - `data`: Inner data used in this runner.
+    /// - `input`: The input data to process.
+    /// - `context`: Mutable reference to the a context.
     fn run(
         &self,
         data: InnerData,
@@ -33,6 +44,46 @@ where
     }
 }
 
+/// `FnFlow` takes some async function and wraps around it to crate a node.
+///
+/// This flow allows for setting custom [`Description`]
+/// through the [`FnFlow::with_description`] function,
+/// since there is no other way to get it (like with [`Node::describe`]).
+///
+/// # Type Parameters
+/// - `Input`: The type of data accepted by this flow.
+/// - `Output`: The type of data produced by this flow.
+/// - `Error`: The type of error emitted by this flow.
+/// - `Context`: The type of context used during execution.
+///
+/// # Examples
+/// ```
+/// use node_flow::flows::FnFlow;
+/// use node_flow::node::{Node, NodeOutput};
+///
+/// #[derive(Clone)]
+/// struct SomeExpensiveData(Vec<u32>);
+///
+/// # tokio::runtime::Builder::new_current_thread()
+/// #     .enable_all()
+/// #     .build()
+/// #     .unwrap()
+/// #     .block_on(async {
+/// async fn main() {
+///     let mut flow = FnFlow::<u32, u32, (), _>::new(
+///         SomeExpensiveData((0..1<<15).collect()),
+///         async |SomeExpensiveData(data), input, _: &mut _| {
+///             let res = data.iter().sum::<u32>() / data.len() as u32 + input;
+///             Ok(NodeOutput::Ok(res))
+///         },
+///     );
+///
+///     let result = flow.run(1, &mut ()).await;
+///     assert_eq!(result, Ok(NodeOutput::Ok(1<<14)));
+/// }
+/// # main().await;
+/// # });
+/// ```
 pub struct FnFlow<Input, Output, Error, Context, InnerData = (), R = ()> {
     #[expect(clippy::type_complexity)]
     _ioec: std::marker::PhantomData<fn() -> (Input, Output, Error, Context)>,
@@ -46,7 +97,49 @@ where
     Input: Send,
     Context: Send,
 {
-    // TODO: mention signature issue in docs (&mut Context must be present, it needs to be async closure and inner data must also be present: async |data: ..., _, _: &mut Context| {...})
+    /// Creates a new [`FnFlow`] from a given runner.
+    ///
+    /// The runner must satisfy:
+    /// - `Self`: `Runner<'_, Input, Output, Error, _, InnerData>`
+    ///
+    /// When using closure as a runner it always needs:
+    /// - to be an **async closure** - because of lifetimes
+    /// - *for context to*:
+    ///     - have the **type of a context** specified when **using** context - because it cannot infer the type\
+    ///       *or*
+    ///     - have the context specified as `_: &mut _` when **not using** context - because it cannot satisfy that `Runner` is implemented
+    /// - to have the **type of inner data** specified when **using** inner data - because it cannot infer the type
+    ///
+    /// # Examples
+    /// ```
+    /// # use node_flow::flows::FnFlow;
+    /// # use node_flow::node::{Node, NodeOutput};
+    /// # use node_flow::context::Fork;
+    /// # #[derive(Clone)]
+    /// struct SomeData(u16);
+    /// # struct Context;
+    /// # impl Fork for Context { fn fork(&self) -> Self { Self } }
+    ///
+    /// FnFlow::<u8, u16, (), Context>::new(
+    ///     SomeData(15),
+    ///     async |_, _, _: &mut _| {
+    ///         Ok(NodeOutput::Ok(30))
+    ///     },
+    /// );
+    /// FnFlow::<u8, u16, (), Context>::new(
+    ///     SomeData(15),
+    ///     async |data: SomeData, _, _: &mut _| {
+    ///         Ok(NodeOutput::Ok(data.0 + 30))
+    ///     },
+    /// );
+    /// FnFlow::<u8, u16, (), Context>::new(
+    ///     SomeData(15),
+    ///     async |_, _, ctx: &mut Context| {
+    ///         let _forked_ctx = ctx.fork();
+    ///         Ok(NodeOutput::Ok(30))
+    ///     },
+    /// );
+    /// ```
     pub fn new<InnerData, R>(
         inner_data: InnerData,
         runner: R,
@@ -67,6 +160,23 @@ where
 impl<Input, Output, Error, Context, InnerData, R>
     FnFlow<Input, Output, Error, Context, InnerData, R>
 {
+    /// Attaches a custom [`Description`] to this flow.
+    ///
+    /// This is be useful when something complex happens in the flow.
+    ///
+    /// # Examples
+    /// ```
+    /// use node_flow::flows::FnFlow;
+    /// use node_flow::describe::{Description, DescriptionBase};
+    /// use node_flow::node::NodeOutput;
+    ///
+    /// let desc = Description::Node {
+    ///     base: DescriptionBase::from::<(), String, usize, u8, u32>()
+    /// }.with_description("I am lying about my types! But shh...");
+    /// let flow = FnFlow::<u8, u16, (), ()>::new((), async|_, x, _: &mut _| {
+    ///     Ok(NodeOutput::Ok(x as u16))
+    /// }).with_description(desc);
+    /// ```
     #[must_use]
     pub fn with_description(mut self, description: Description) -> Self {
         self.runner_description = Some(std::sync::Arc::new(description));
